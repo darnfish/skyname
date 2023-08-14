f543ab8bcc176b4e57112db1fa332963401809ee
William Gibson <william@darn.fish>
2023-08-14 03:03:32 -0400

    Don't enforce any database flags

--- lib/knex.ts
+++ lib/knex.ts
@@ -1,10 +1,8 @@
 import Knex from 'knex'
 
-const url = `${process.env.POSTGRES_URL}?sslmode=require`
-
 const knex = Knex({
 	client: 'pg',
-	connection: url
+	connection: process.env.POSTGRES_URL
 })
 
 export default knex
