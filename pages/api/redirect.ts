f54edcfb1f382c369af315ce7b3c1a3ac61bab96
William Gibson <william@darn.fish>
2023-11-10 12:59:09 -0500

    Redirect to profile if exists

--- pages/api/redirect.ts
+++ pages/api/redirect.ts
@@ -1,10 +1,27 @@
+import knex from 'knex'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(
-  request: NextApiRequest,
-  response: NextApiResponse,
+	request: NextApiRequest,
+	response: NextApiResponse,
 ) {
-  response.status(200).json({
-    headers: request.headers
-  })
+	/**
+	 * Check if it's not a GET
+	 */
+	if(request.method !== 'GET') {
+		response.status(405).json({ error: 'Method is not GET' })
+		return
+	}
+
+	const host = request.headers.host
+	const hostParts = host.split('.')
+ 
+	const domain = [hostParts.at(-2), hostParts.at(-1)].join('.')
+	const subdomain = hostParts.slice(0, -2).join('.')
+	
+	const rows = await knex('registrations').where({ subdomain, domain }).whereNull('invalidated_at')
+	if(rows.length === 0)
+		return response.redirect('https://skyna.me')
+
+	response.redirect(`https://bsky.app/profile/${subdomain}.${domain}`)
 }
